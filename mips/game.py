4ed30acf47e80ad400086dc903f0bc5c5860041a
Nikola Kolevski <nikola.kolevski@tradecore.com>
2017-08-23 19:02:47 +0200

    Lint and add development requirements

--- mips/game.py
+++ mips/game.py
@@ -1,38 +1,43 @@
+from random import random
+
 import pygame, sys, time
 from pygame.locals import QUIT, KEYDOWN, K_LEFT, K_RIGHT, K_UP, K_DOWN
-from random import random
 
 
 def mistake():
-    return random()*10 - 5 # between -5 and 5
+    """Return a random number between -5 and 5."""
+    return random()*10 - 5
+
+
+FPS = 30
+
+
+UP = 'up'
+LEFT = 'left'
+RIGHT = 'right'
+DOWN = 'down'
 
 
 def run():
+    """Main game loop."""
     pygame.init()
 
-    FPS = 30
-    fpsClock = pygame.time.Clock()
+    fps_clock = pygame.time.Clock()
 
     width = 700
     height = 500
-    DISPLAYSURF = pygame.display.set_mode((width,height),0,32)
+    displaysurf = pygame.display.set_mode((width, height), 0, 32)
     pygame.display.set_caption('Animation')
     background = pygame.image.load('bg.png')
-
-    UP = 'up'
-    LEFT = 'left'
-    RIGHT = 'right'
-    DOWN = 'down'
 
     sprite = pygame.image.load('down.png')
     spritex = 0
     spritey = 0
-    direction = DOWN
 
     while True:
-        DISPLAYSURF.blit(background,(0,0))
+        displaysurf.blit(background, (0, 0))
 
-        DISPLAYSURF.blit(sprite,(spritex,spritey))
+        displaysurf.blit(sprite, (spritex, spritey))
 
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -41,20 +46,20 @@ def run():
 
             if event.type == KEYDOWN:
                 if event.key == K_LEFT:
-                    spritex-=70 + mistake()
+                    spritex -= 70 + mistake()
                     # sprite=pygame.image.load('left.png')
                 elif event.key == K_RIGHT:
-                    spritex+=70 + mistake()
+                    spritex += 70 + mistake()
                     # sprite=pygame.image.load('right.png')
                 elif event.key == K_UP:
-                    spritey-=50 + mistake()
+                    spritey -= 50 + mistake()
                     # sprite=pygame.image.load('up.png')
                 elif event.key == K_DOWN:
-                    spritey+=50 + mistake()
+                    spritey += 50 + mistake()
                     # sprite=pygame.image.load('down.png')
 
         pygame.display.update()
-        fpsClock.tick(FPS)
+        fps_clock.tick(FPS)
 
 
 if __name__ == "__main__":
